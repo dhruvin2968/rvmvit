e3b8f94113f297bd440029a0c91449357632b064
Dhruvin <therockstar3007@gmail.com>
2024-12-25 02:18:07 +0530

    PgNotFound3

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -16,9 +16,14 @@ export const PageNotFound = () => {
             <img className="rounded" src="/pagenotfound.png" alt="404 Page Not Found" />
           </div>
         </div>
-        <Link to="">
-        <button> Back To RevMechanics</button> 
-        </Link>
+        <div className="flex items-center justify-center">
+          <Link 
+            to="/"
+            className="px-6 py-3 bg-gradient-to-r from-red-500 to-yellow-500 text-white font-semibold rounded-lg shadow-lg hover:bg-red-700 transform transition duration-300"
+          >
+            Back To RevMechanics
+          </Link>
+        </div>
       </section>
     </main>
   )
