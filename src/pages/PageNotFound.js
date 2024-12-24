562fa6be90cfd25c9faab10da8765ee016eff568
Dhruvin <therockstar3007@gmail.com>
2024-12-25 02:04:36 +0530

    PgNotFound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export const PageNotFound = () => {
+
+  useEffect(() => {
+    document.title = `Page Not Found / Cinemate`;
+  });
+
   return (
-    <div>PageNotFound</div>
+    <main>
+      <section className="flex flex-col justify-center px-2">
+        <div className="flex flex-col items-center my-4">
+          <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white">404, Oops!</p>
+          <div className="max-w-lg">
+            <img className="rounded" src="/pagenotfound.png" alt="404 Page Not Found" />
+          </div>
+        </div>
+        <Link to="">
+        <button> Back To RevMechanics</button> 
+        </Link>
+      </section>
+    </main>
   )
 }
